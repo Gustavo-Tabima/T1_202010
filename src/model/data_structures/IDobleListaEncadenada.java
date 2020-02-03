0c334c97437e6636895e81e656e8542f25407700
jmRiveral <jm.riveral@uniandes.edu.co>
2020-02-02 21:07:07 -0500

    .

---
+++ src/model/data_structures/IDobleListaEncadenada.java
@@ -0,0 +1,13 @@
+package model.data_structures;
+
+import java.util.Iterator;
+
+public interface IDobleListaEncadenada extends Iterable { 
+
+	public int darTamaño();
+	public void instertarInicio();// metodo para instertar dato como primero de la lista.
+	public void instetarFinal(); // metodo para insertar dato al final de la lista.
+	public void instertarMed(); // metodo para instertar en cualcquier otra posiscion
+	public void 
+
+}
