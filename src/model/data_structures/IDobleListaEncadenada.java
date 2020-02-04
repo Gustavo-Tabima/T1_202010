d7f54746ff4de565b17edfa8d1440c87372fc308
Gustavo-Tabima <=>
2020-02-04 11:40:22 -0500

    Creada una interfaz generica temporalmente (falta revisarlo)

--- src/model/data_structures/IDobleListaEncadenada.java
+++ src/model/data_structures/IDobleListaEncadenada.java
@@ -1,25 +1,25 @@
 package model.data_structures;
 
-import java.util.Iterator;
 
-public interface IDobleListaEncadenada  extends Iterable { 
 
-	public int darTamaño();
+public interface IDobleListaEncadenada <Z extends Iterable<Z>>   { 
+
+	public Z darTamaño();
 	
-	public String darPrimerElemento();// metodo para instertar dato como primero de la lista.
+	public Z darPrimerElemento();// metodo para instertar dato como primero de la lista.
 	
-	public String darUltimoElemento(); // metodo para insertar dato al final de la lista.
-	public String darElemento();
+	public Z darUltimoElemento(); // metodo para insertar dato al final de la lista.
+	public Z darElemento();
 	
-	public String BuscarElemneto(String ID);
+	public Z BuscarElemneto(String ID);
 	
 	public void limpiarLista(); // metodo para instertar en cualcquier otra posiscion
 	
-	public void agregarDespues(String ID); // metodo para agregar despues de cierto objeto
+	public void agregarDespues(Z ID); // metodo para agregar despues de cierto objeto
 	
-	public void agregarPrimero(Comparendo pComparendo);// agrega comp primer objeto de la lista
+	public void agregarPrimero(Z pComparendo);// agrega comp primer objeto de la lista
 	
-	public void agregarUltimo(Comparendo nuevoComparendo); //agrega como ultimo  objeto de la lista
+	public void agregarUltimo(Z nuevoComparendo); //agrega como ultimo  objeto de la lista
 	
 	public void removerPrimero(); // remueve el primer objeto de la lista
 	
@@ -27,6 +27,8 @@ public interface IDobleListaEncadenada  extends Iterable {
 	public void removerUltimo();// remueve el ultimo objeto de la lista
 	
 	public void remover(String ID); // remueve un objeto especifico de la lista
+
+	
 
 	
 
